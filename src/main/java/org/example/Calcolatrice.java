8121055e987b3177cb57403b20b1fc9d81e272bb
“nicolamoothreemodugno” <nmodugno48@gmail.com>
2022-10-05 16:11:16 +0200

    Aggiunta funzione sottrazione

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -4,4 +4,8 @@ public class Calcolatrice {
     public double somma(double a, double b){
         return a+b;
     }
+
+    public double sottrazione(double a, double b){
+        return a-b;
+    }
 }
